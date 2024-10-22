5971f5a541bf18654c7e86151b26ff378149c77b
Tom Kimpson <tomkimpson@gmail.com>
2024-10-22 11:38:34 +1100

    Clean up UKF implementation, remove old code

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -3,6 +3,17 @@ import scipy
 import sys
 
 
+"""
+Given the innovation and innovation covariance, get the likelihood.
+Shared by multiple classes
+"""
+def log_likelihood(y,cov):
+    N = len(y)
+    sign, log_det = np.linalg.slogdet(cov)
+    ll = -0.5 * (log_det + np.dot(y.T, np.linalg.solve(cov, y))+ N*np.log(2 * np.pi))
+    return ll
+
+
 class ExtendedKalmanFilter:
     """
     A class to implement the extended (non-linear) Kalman filter.
@@ -168,7 +179,9 @@ class UnscentedKalmanFilter:
     """
     A class to implement the Unscented Kalman filter (UKF).
 
-    It takes four initialisation arguments:
+    Please see Wan & Van Der Merwe, "The unscented Kalman filter for nonlinear estimation" https://ieeexplore.ieee.org/document/882463. W&V2000 hereafter
+
+    It takes seven initialisation arguments:
 
         `Model`: class which defines all the Kalman machinery e.g. state transition models, covariance matrices etc. 
 
@@ -176,41 +189,39 @@ class UnscentedKalmanFilter:
 
         `x0`: A 1D array which holds the initial guess of the initial states
 
-        `P0`: The uncertainty in the guess of P0
+        `P0`: The uncertainty in the guess of x0
 
-    ...and a general **kwargs that can be modified for the specific problem. todo
+        `α`: a float used to determine the spread of the sigma points, usually a small number
+        
+        `β`: a float used to incrporate prior knowledge on distribution of the states. For Gaussians, use β=2
 
-
-    The states are passed mix of functional and OOP, the states are passed as arguments i.e. no self.x
-    just makes it easier to track where they are updated
-    todo
+        `κ`: a secondary scaling parameters, usually zero
 
     """
 
-    def __init__(self,Model,Observations,x0,P0,α,β,κ,**kwargs):
+    def __init__(self,Model,Observations,x0,P0,α=1e-3,β=2.0,κ=0):
 
         """
         Initialize the class. 
         """
 
    
+        #Load the mathematical model and the empirical observations
         self.model        = Model
         self.observations = Observations
-
-
-   
-        assert self.observations.ndim == 2, f'This filter requires that input observations is a 2D array. The observations here have {self.observations.ndim} dimensions '
+        assert self.observations.ndim == 2, f'This filter requires that input observations is a 2D array. The observations here have {self.observations.ndim} dimensions ' # even if the observations are a scalar timeseries, we require this to be an array (n_steps,1)
       
      
+        # Define some useful quantities related to the dimension of the problem
         self.dt = self.model.dt
         self.n_measurement_states  = self.observations.shape[-1] #number of measurement states
         self.n_steps               = self.observations.shape[0]  #number of observations/timesteps
         self.n_states              = self.model.n_states
 
 
- 
-        self.x0 = x0 #Guess of the initial state 
-        self.P0 = P0 #Guess of the initial state covariance
+        #Guess of the initial state and the uncertainty in that guess
+        self.x0 = x0 
+        self.P0 = P0 
 
         # parameters of the UKF
         self.α = α
@@ -218,11 +229,11 @@ class UnscentedKalmanFilter:
         self.κ = κ
 
     """
-    What does this funciton do exactly..... 
+    Calculate the sigma points weight vectors, see Equation (15) from W&V2000
     """
     def CalculateWeights(self):
 
-        # Compute sigma point weights
+        # First define the scaling parameter
         λ = self.α**2 * (self.n_states + self.κ) - self.n_states
 
 
@@ -230,21 +241,27 @@ class UnscentedKalmanFilter:
         self.Wc = np.concatenate(([λ/(self.n_states+λ)+(1-self.α**2+self.β)], (0.5/(self.n_states+λ))*np.ones(2*self.n_states)), axis=None)
         self.γ = np.sqrt(self.n_states + λ)
 
+        # Developer note: most other functions in this class are of the form a = f(x), i.e. we don't update the state but return a value
+        # This is done as it makes it a bit easier to read what quantities are being updated where
+        # In contrast for this function we just update the state directly. It is only called once. 
 
-    """_summary_
+
+    """
+    Calculate the sigma points vectors, see Equation (15) from W&V2000
     """
     def CalculateSigmaPoints(self, x, P):
 
 
-  
-        epsilon = 1e-24 #todo, does this value matter?
-        Pos_definite_Check= 0.5*(P + P.T) + epsilon*np.eye(len(x))
+        # Here we enforce the posdef property of the covariance matrix, which can be lost sometimes due to numerical errors
+        # The value of epsilon has been found to work well in practice. Are there circumstances where we will need to modify this?
+        epsilon = 1e-24 
+        positive_definite_check= 0.5*(P + P.T) + epsilon*np.eye(len(x))
+        U               = scipy.linalg.cholesky(positive_definite_check).T # sqrt
         
-        U               = scipy.linalg.cholesky(Pos_definite_Check).T # sqrt
+        
+        # Now calculate the sigma points by Equation (15)
         sigma_points    = np.zeros((2*self.n_states + 1, self.n_states))
-        
         sigma_points[0] = x
-
         for i in range(self.n_states):
             sigma_points[i+1]               = x + self.γ*U[:, i]
             sigma_points[self.n_states+i+1] = x - self.γ*U[:, i]
@@ -252,7 +269,9 @@ class UnscentedKalmanFilter:
 
 
 
-
+    """
+    A fourth order Runge-Kutta integration step
+    """
     def rk4_step(self,x):
         k1 = self.dt * self.model.derivative_function(x, self.g)
         k2 = self.dt * self.model.derivative_function(x + k1 / 2, self.g)
@@ -262,69 +281,80 @@ class UnscentedKalmanFilter:
         return x + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 
+    """
+    Use the RK4 integrator to propagate the sigma points forward by one timestep
+    todo: I think we can vectorise this and combine with the rk4_step function
+    """
     def propagate_sigma_points(self,sigma_points):
-
         propagated_sigma_points = np.empty_like(sigma_points)
         for i, x in enumerate(sigma_points):
             propagated_sigma_points[i] = self.rk4_step(x)
         return propagated_sigma_points
 
 
+    """ 
+    The predict stage of the UKF. See Algorithm 3.1 in  W&V2000
+    """
     def predict(self,propagated_sigma_points):
-
+        # Predict the state
         xp = np.zeros((self.n_states, 1))
         for i in range(len(self.Wm)):
             xp += self.Wm[i] * propagated_sigma_points[i].reshape(self.n_states, 1)
 
             
-        SigmaPointDiff = np.zeros((len(propagated_sigma_points), self.n_states))
+        #Predict the covariance
+        sigma_point_diff = np.zeros((len(propagated_sigma_points), self.n_states))
         for i, point in enumerate(propagated_sigma_points):
-            SigmaPointDiff[i] = point - xp.ravel()
+            sigma_point_diff[i] = point - xp.ravel()
 
         Pp = np.zeros((self.n_states, self.n_states))
-        for i, diff in enumerate(SigmaPointDiff):
+        for i, diff in enumerate(sigma_point_diff):
             Pp += self.Wc[i] * np.outer(diff, diff)
             
         Pp += self.Q
-
         return xp, Pp
 
 
 
-
+    """ 
+    The update stage of the UKF. See Algorithm 3.1 in  W&V2000
+    """
     def update(self,xp, Pp, propagated_sigma_points, observation):
         
-        #todo, update this notatoin
-        SP_Meas = np.column_stack((np.sin(propagated_sigma_points[:, 0])))  # this should come in from th emodel
-        WeightedMeas = np.einsum('i,ij->j', self.Wm, SP_Meas.T)
-        MeasurementDiff = SP_Meas - WeightedMeas
 
-        
-        Inn = observation - WeightedMeas
 
-        SigmaPointDiff = propagated_sigma_points - xp.T
+        # Use your current sigma points to make a prediction and compare that with the actual observation to get the innovation, y
+        sigma_points_in_measurement_space = np.column_stack((self.model.sigma_point_measurement_function(propagated_sigma_points)))
+        y_predicted                       = np.einsum('i,ij->j', self.Wm, sigma_points_in_measurement_space.T) # Equation (17) from W&V2000
+        y                                 = observation - y_predicted
 
-        PredMeasurementCov = np.zeros((self.n_measurement_states, self.n_measurement_states))
+
+        #Calculate the innovation covariance, Equation (18) from from W&V2000
+        measurement_difference = sigma_points_in_measurement_space - y_predicted
+        P_yy = np.zeros((self.n_measurement_states, self.n_measurement_states))
         for i in range(len(self.Wc)):
-            PredMeasurementCov += self.Wc[i] * np.outer((MeasurementDiff.T)[i],( MeasurementDiff.T)[i])
-        PredMeasurementCov += self.R
+            P_yy += self.Wc[i] * np.outer((measurement_difference.T)[i],(measurement_difference.T)[i]) # Equation (18) from W&V2000
+        P_yy += self.R
 
-        CrossCovariance = np.zeros((self.n_states, self.n_measurement_states))
+
+
+
+        #Calculate the cross-covariance 
+        states_difference = propagated_sigma_points - xp.T
+        P_xy = np.zeros((self.n_states, self.n_measurement_states))
         for i in range(len(self.Wc)):
-            CrossCovariance += self.Wc[i] * np.outer(SigmaPointDiff[i], (MeasurementDiff.T)[i])
+            P_xy += self.Wc[i] * np.outer(states_difference[i], (measurement_difference.T)[i])
 
-        kalman_gain = np.linalg.solve(PredMeasurementCov.T, CrossCovariance.T).T
 
-        x = xp + kalman_gain*Inn
+        # Calculate the Kalman gain and update the state and covariance estimates
+        kalman_gain = np.linalg.solve(P_yy.T, P_xy.T).T #todo, check all these transposes. 
+        x           = xp + kalman_gain*y
+        P           = Pp - np.dot(kalman_gain, np.dot(P_yy, kalman_gain.T))
 
-        P = Pp - np.dot(kalman_gain, np.dot(PredMeasurementCov, kalman_gain.T))
 
-        sign, log_det = np.linalg.slogdet(PredMeasurementCov)
-        ll = -0.5 * (log_det + np.dot(Inn.T, np.linalg.solve(PredMeasurementCov, Inn))
-                    + np.log(2 * np.pi))
-        
-        return x.flatten(), P, ll, np.sin(x[0]) #need the measurement model here
-
+        #and finally calculate the likelihood for this step
+        ll = log_likelihood(y,P_yy)
+        return x.flatten(), P, ll, self.model.measurement_function(x) 
 
 
 
@@ -334,7 +364,15 @@ class UnscentedKalmanFilter:
 
 
 
-    
+
+    """ 
+    Run the UKF given a set of free parameters
+    Updates 
+        state_predictions
+        measurement_predictions
+        state_covariance
+        ll (likelihood)
+    """
     def run(self,parameters):
 
 
